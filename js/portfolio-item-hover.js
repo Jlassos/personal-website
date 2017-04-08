3d58594080bb483dbace3789989bbb9d329411ee
James <james@Tassadar.local>
2017-04-07 17:20:36 -0700

    portfolio js things

--- js/portfolio-item-hover.js
+++ js/portfolio-item-hover.js
@@ -1,6 +1,8 @@
 var portfolio = {
   A: {
     githubLink: 'https://github.com/Jlassos/box-buddy',
+    header: 'Box Buddy',
+    subheader: 'Virtual Pet',
     imageSrc: '/img/box-buddy.png',
     description: 'This is so rad, just look at me killin it!',
     details: [
@@ -9,6 +11,8 @@ var portfolio = {
     ]
   },
   B: {
+    header: 'Red Rover',
+    subheader: 'Application',
     imageSrc: '/img/rr-mark-red.png',
     description: 'Also did this, I know, so rad.',
     details: [
@@ -23,11 +27,13 @@ var showProject = function (projectData) {
   var $display = document.querySelector('.display')
 
   var $image = $display.querySelector('.img-responsive')
+  var $header = document.querySelector('.item-header')
   var $description = document.querySelector('.description')
   var $details = document.querySelector('.details')
-  var $button = $display.querySelector('a')
+  // var $button = $display.querySelector('a')
 
   $image.src = projectData.imageSrc
+  $header.innerText = projectData.header
   $description.innerText = projectData.description
   $details.innerText = projectData.details
   // $button.href = projectData.githubLink
